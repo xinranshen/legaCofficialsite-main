2ebb16533407b3d88652568ac5c55f171d82839b
xinranshen <xs1115@nyu.edu>
2021-12-12 14:13:32 -0500

    mvmvp

--- main.js
+++ main.js
@@ -11,11 +11,6 @@
 // })
 
 
-window.addEventListener('scroll', reveal);
-
-function reveal(){
- var reveals = document.querySelectorAll('.reveal');
-
  for(var i = 0; i < reveals.length; i++){
 
    var windowheight = window.innerHeight;
